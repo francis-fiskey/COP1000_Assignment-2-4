f2b67a4ad8a36883550cf24e0cbde360e53e174c
francis-fiskey <7523740+francis-fiskey@users.noreply.github.com>
2024-02-16 18:19:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# input statements
+salary = float(input())
+numDependents = float(input())
+
+# calculate taxes here
+
+# output statements
+print("Salary: $" + str(salary))
+print("Take Home Pay: $" + str(takeHomePay))
